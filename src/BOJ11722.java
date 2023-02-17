9ced1a4e643cb7719e97c49148fe3fa268e69630
bye-on <codnjs842@naver.com>
2023-02-17 18:29:54 +0900

    Solve: Re-try

---
+++ src/BOJ11722.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11722 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int[] num = new int[n];
+        String[] str = bf.readLine().split(" ");
+        int[] cnt = new int[n];
+        int result = 1;
+        for(int i = 0; i < n; i++) {
+            cnt[i] = 1;
+            num[i] = Integer.parseInt(str[i]);
+            for(int j = 0; i > j; j++) {
+                if(num[i] < num[j]) {
+                    cnt[i] = Math.max(cnt[i], cnt[j] + 1);
+                }
+            }
+            result = Math.max(result, cnt[i]);
+        }
+        System.out.println(result);
+    }
+}
+
